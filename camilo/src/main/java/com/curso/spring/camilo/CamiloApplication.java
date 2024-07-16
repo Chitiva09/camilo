0c5d386297d158a37b8a08ba7fecf67d34e18c46
Chitiva09 <bermeocamilo12@gmail.com>
2024-07-16 15:59:00 -0500

    First commit

---
+++ camilo/src/main/java/com/curso/spring/camilo/CamiloApplication.java
@@ -0,0 +1,13 @@
+package com.curso.spring.camilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamiloApplication.class, args);
+	}
+
+}
---
+++ camilo/src/test/java/com/curso/spring/camilo/CamiloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.spring.camilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
